aec0b09c17d76dc4f6977f99a2e5ca901696897c
Charles <ystanglei@gmail.com>
2018-07-03 14:17:10 +0800

    Demo

---
+++ Example/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Charles on 2018/6/29.
+//  Copyright © 2018 Charles. All rights reserved.
+//
+
+import UIKit
+import TLCommon
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let textView = UITextView(frame: CGRect(x: 0, y: 100, width: 375, height: 100))
+        
+        textView.font = UIFont.systemFont(ofSize: 24)
+//        textView.text = "hello"
+//        textView.placeholder = "爱谁谁爱谁谁爱谁谁爱谁谁爱谁谁爱谁谁爱谁谁爱谁谁爱谁谁爱谁谁爱谁谁爱谁谁"
+        
+        
+        view.addSubview(textView)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
