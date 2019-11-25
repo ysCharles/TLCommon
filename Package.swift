c0dc4337960459312dca2aab0e6e48d1191ba961
Charles <ystanglei@gmail.com>
2019-11-25 13:52:37 +0800

    SPM

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "TLCommon",
+	platforms:[.iOS(.v10))],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -20,6 +21,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "TLCommon",
-            dependencies: []),
+            path: "Sources"),
     ]
 )
